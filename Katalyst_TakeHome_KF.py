cb69f7f3672c96cbe810f047628e8059660cb3c9
rherberg12 <167365676+rherberg12@users.noreply.github.com>
2025-01-30 08:20:07 -0700

    Add files via upload

--- Katalyst_TakeHome_KF.py
+++ Katalyst_TakeHome_KF.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Read in measurements
+mu = 3.986*10**5 # km^3/s^2
 df = pd.read_csv('measurements.csv', index_col = None, header = 'infer')
 
 # Organize measurements to each respective source
@@ -74,10 +75,25 @@ def EKF(dk, z_k, x_k_minus_1, u_k_minus_1, P_k_minus_1, H_k, L_k, R_k, eom_fun, 
 
     return [x_k,P_k]
 
+def eom_J2_perturbation(t, X, vars):
+    J2 = 1082.64E-6
+    a = vars
+
+    r_vec = X[:3]
+    r_norm = np.linalg.norm(r_vec)
+    v_vec = X[3:6]
+
+    aJ2 = [[(-mu*r_vec[0]/r_norm**3)*(1+(3/2)*J2*(a**2/r_norm**2)-(15/2)*J2*((a**2*r_vec[2]**2)/r_norm**4))],
+           [(-mu*r_vec[1]/r_norm**3)*(1+(3/2)*J2*(a**2/r_norm**2)-(15/2)*J2*((a**2*r_vec[2]**2)/r_norm**4))],
+           [(-mu*r_vec[2]/r_norm**3)*(1+(9/2)*J2*(a**2/r_norm**2)-(15/2)*J2*((a**2*r_vec[2]**2)/r_norm**4))]]
+
+    dstate = np.array([v_vec,-mu*r_vec/r_norm**3 + aJ2])
+
+    return dstate
+
 
 def eom_orbit_controlled_ECI_w_covariance(t, X, vars):
     # X[0:7] is state position and velocity, X[7:end] is covariance matrix (6x6)
-    mu = 3.986*10**5 # km^3/s^2
     Qs = vars[0] # State model noise covariance matrix
     M = vars[1] # Noise covariance matrix mapping matrix
     u = vars[2] # Control vector
@@ -113,7 +129,7 @@ def rk4(fun,dt,tk,xk,fun_vars): # fun_vars includes the control input and variab
     xout = xk + (dt/6)*(f1+2*f2+2*f3+f4)
     return xout
 
-def gps_EKF_calculation():
+def EKF_calculation():
     # Operate kalman filter through measurements to define estimates state vector
     t0 = gps_measurements.iloc[0,0] # Initial time
     u_k_minus_1 = eom_options_gps[2] # Assumption 1: no control
@@ -123,8 +139,8 @@ def gps_EKF_calculation():
     L_k_minus_1 = L_gps
     R_k_minus_1 = R_gps
     eom_options_k_minus_1 = eom_options_gps
-    x_array = np.array(gps_measurements.iloc[0,1:])
-    time_array = np.array([t0])
+    x_array_gps = np.array(gps_measurements.iloc[0,1:])
+    time_array_gps = np.array([t0])
 
     for k in range(len(gps_measurements.index)-1):
         dk = gps_measurements.iloc[k+1,0] - gps_measurements.iloc[k,0] # Time step
@@ -137,27 +153,76 @@ def gps_EKF_calculation():
             x_k_minus_1 = x_estimate_k
             P_k_minus_1 = P_estimate_k
         x_estimate_k, P_estimate_k = EKF(dk,z_k_minus_1,x_k_minus_1,u_k_minus_1,P_k_minus_1,H_k_minus_1,L_k_minus_1,R_k_minus_1,eom_orbit_controlled_ECI_w_covariance,eom_options_k_minus_1)
-        x_array = np.append(x_array,x_estimate_k[:])
-        time_array = np.append(time_array,time_array[-1]+dk)
+        x_array_gps = np.append(x_array_gps,x_estimate_k[:])
+        time_array_gps = np.append(time_array_gps,time_array_gps[-1]+dk)
+    x_array_gps = x_array_gps.reshape(len(time_array_gps),6)
+
+    # Identify the relevant orbital elements from the gps
+    E_gps_est = np.zeros(len(x_array_gps))
+    a_gps_est = np.zeros(len(x_array_gps))
+    for i in range(len(x_array_gps)):
+        E_gps_est[i] = (np.linalg.norm(x_array_gps[i][3:])/2)-(mu/np.linalg.norm(x_array_gps[:3]))
+        a_gps_est[i] = -mu/(2*E_gps_est)
+    E_gps_est = np.mean(E_gps_est)
+    a_gps_est = np.mean(a_gps_est)
+
+    # Propogate through period of ground observer observation to correct position.           
+
+    time_go1 = np.arange(time_go10,ground_observer_1[-1][0],5) # 5 sec step size
+    x_prop_go1 = np.zeros(len(time_go1))
+    x_prop_go1
+    for t in range(len(time_go1)):
+        x_prop_go1[t+1][:] = rk4(eom_J2_perturbation, time_go1[t+1]-time_go1[t], time_go1[t], x_prop_go1[t],a_gps_est)
+
+
+    for k in range(len(ground_observer_1.index)-1):
+        dk = ground_observer_1.iloc[k+1,0] - ground_observer_1.iloc[k,0] # Time step
+        z_k_minus_1 = ground_observer_1.iloc[k,1:]
+        if k ==0: # Initialization
+            x_k_minus_1 = ground_observer_1.iloc[k,1:]
+            P_k_minus_1 = np.eye(6)*np.array([[0.1**2],[0.1**2],[0.1**2],[0.01**2],[0.01**2],[0.01**2]])*1E6 # Assume large initial uncertainty 
+            P_k_minus_1 = P_k_minus_1.reshape((len(ground_observer_1.iloc[0,1:])*len(ground_observer_1.iloc[0,1:])),1)
+        else:
+            x_k_minus_1 = x_estimate_k
+            P_k_minus_1 = P_estimate_k
+        x_estimate_k, P_estimate_k = EKF(dk,z_k_minus_1,x_k_minus_1,u_k_minus_1,P_k_minus_1,H_k_minus_1,L_k_minus_1,R_k_minus_1,eom_orbit_controlled_ECI_w_covariance,eom_options_k_minus_1)
+        x_array_go1 = np.append(x_array_go1,x_estimate_k[:])
+        time_array_go1 = np.append(time_array_go1,time_array_go1[-1]+dk)
+
+    for k in range(len(ground_observer_2.index)-1):
+        dk = ground_observer_2.iloc[k+1,0] - ground_observer_2.iloc[k,0] # Time step
+        z_k_minus_1 = ground_observer_2.iloc[k,1:]
+        if k ==0: # Initialization
+            x_k_minus_1 = ground_observer_2.iloc[k,1:]
+            P_k_minus_1 = np.eye(6)*np.array([[0.1**2],[0.1**2],[0.1**2],[0.01**2],[0.01**2],[0.01**2]])*1E6 # Assume large initial uncertainty 
+            P_k_minus_1 = P_k_minus_1.reshape((len(ground_observer_2.iloc[0,1:])*len(ground_observer_2.iloc[0,1:])),1)
+        else:
+            x_k_minus_1 = x_estimate_k
+            P_k_minus_1 = P_estimate_k
+        x_estimate_k, P_estimate_k = EKF(dk,z_k_minus_1,x_k_minus_1,u_k_minus_1,P_k_minus_1,H_k_minus_1,L_k_minus_1,R_k_minus_1,eom_orbit_controlled_ECI_w_covariance,eom_options_k_minus_1)
+        x_array_go2 = np.append(x_array_go2,x_estimate_k[:])
+        time_array_go2 = np.append(time_array_go2,time_array_go2[-1]+dk)
     
     # Post-Process
-    ax = plt.figure().add_subplot(projection='3d')
-    x_array = x_array.reshape(len(time_array),6)
-    x_array = pd.DataFrame(x_array, index = time_array, columns=['r_x_km','r_y_km','r_z_km','v_x_km/s','v_y_km/s','v_z_km/s'])
-    gps_measurements.set_index('time',inplace=True)
-    correction = x_array.subtract(gps_measurements)
-    a_gps = np.zeros((len(gps_measurements),1))
-    a_EKF = np.zeros((len(gps_measurements),1))
-    for i in range(len(a_gps)):
-        a_gps[i] = np.sqrt(gps_measurements.iloc[i,0]**2 + gps_measurements.iloc[i,1]**2 + gps_measurements.iloc[i,2]**2)
-        a_EKF[i] = np.sqrt(x_array.iloc[i,0]**2 + x_array.iloc[i,1]**2 + x_array.iloc[i,2]**2)
-    ax.plot(gps_measurements['r_x_km'],gps_measurements['r_y_km'],gps_measurements['r_z_km'], label = 'gps')
-    ax.plot(x_array['r_x_km'],x_array['r_y_km'],x_array['r_z_km'],label = 'EKF')
-    plt.legend()
-    plt.style.use('ggplot')
-    plt.show()
+    # x_array_gps = pd.DataFrame(x_array_gps, index = time_array_gps, columns=['r_x_km','r_y_km','r_z_km','v_x_km/s','v_y_km/s','v_z_km/s'])
+    # gps_measurements.set_index('time',inplace=True)
+    # ax = plt.figure().add_subplot(projection='3d')
+    # x_array = x_array.reshape(len(time_array),6)
+    # x_array = pd.DataFrame(x_array, index = time_array, columns=['r_x_km','r_y_km','r_z_km','v_x_km/s','v_y_km/s','v_z_km/s'])
+    # gps_measurements.set_index('time',inplace=True)
+    # correction = x_array.subtract(gps_measurements)
+    # a_gps = np.zeros((len(gps_measurements),1))
+    # a_EKF = np.zeros((len(gps_measurements),1))
+    # for i in range(len(a_gps)):
+    #     a_gps[i] = np.sqrt(gps_measurements.iloc[i,0]**2 + gps_measurements.iloc[i,1]**2 + gps_measurements.iloc[i,2]**2)
+    #     a_EKF[i] = np.sqrt(x_array.iloc[i,0]**2 + x_array.iloc[i,1]**2 + x_array.iloc[i,2]**2)
+    # ax.plot(gps_measurements['r_x_km'],gps_measurements['r_y_km'],gps_measurements['r_z_km'], label = 'gps')
+    # ax.plot(x_array['r_x_km'],x_array['r_y_km'],x_array['r_z_km'],label = 'EKF')
+    # plt.legend()
+    # plt.style.use('ggplot')
+    # plt.show()
 
-gps_EKF_calculation()
+EKF_calculation()
 
     
 
